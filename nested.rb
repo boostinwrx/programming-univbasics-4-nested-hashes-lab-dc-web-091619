40b6f76f320b15faf22222c1875a9962cba8f73f
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-06 20:34:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
-return programmer_hash[:dennis_ritchie][:languages]
+    
+p programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
